51197ffdece764738082db3e3ff72c7312b04950
Nam39rou7d <https://github.com/Namegorund>
2023-09-08 18:03:12 +0900

    20230908 수업내용

--- Java01/src/j07_classExtends/Ex01_classNclass.java
+++ Java01/src/j07_classExtends/Ex01_classNclass.java
@@ -9,27 +9,32 @@ package j07_classExtends;
 public class Ex01_classNclass {
 
 	static Ex01_Car car = new Ex01_Car(100, 200, "Yellow"); // 선언과 동시에 초기화
-	
+
 	public static void myCar(Ex01_Car car) {
 		car.speedDown();
 		System.out.println("2) 사용(use) => myCar_speed => " + car.speed);
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		// 1) 집합(has-a)
 		// => Ex01_classNclass 클래스와 Ex01_Car 클래스 관계
 		System.out.println("1) 집합(has-a) => " + car);
-		
+
 		// => Student 와 Car 클래스의 관계
 		Ex01_Student s1 = new Ex01_Student("0001", "홍길동", 99);
 		System.out.println("Main Student => " + s1);
 		System.out.println("Main Car_color => " + s1.car.color);
 		System.out.println("Main myCar_color => " + Ex01_Student.myCar.color.toUpperCase());
-		
+
 		// 2) 사용(use)  
 		// => Ex01_classNclass 클래스가 메서드 인자의 형식으로 Ex01_Car car 를 사용한 경우 => use의 관계
 		myCar(car);
+
+		// 3) 상속(is-a)
+		// => 후손과 조상이 같은 Type 이 될 수 있다.
+		// => Ex01_Car 와 SportsCar
+		// => Ex02_CarTest.java (참고)
 
 	}
 
---
+++ Java01/src/j08_absInterface/Ex03_MyInterTest.java
@@ -0,0 +1,78 @@
+package j08_absInterface;
+
+class MyChild1 implements Ex03_MyInterface {
+	
+	// 생성자
+	public MyChild1() { 
+		System.out.println("MyChild1 default 생성자");
+	}
+	
+	@Override
+	public String getName() {
+		return NAME;
+	}
+	@Override
+	public int getNum() {
+		return NUM;
+	}
+	
+	void child1() {
+		System.out.println("MyChild1 child1() 메서드 ");
+	}
+}
+class MyChild2 implements Ex03_MyInterface {
+	
+	String name = "홍길동";
+	int num = 100;
+	
+	// 생성자
+	public MyChild2() { 
+		System.out.println("MyChild2 default 생성자");
+	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+	@Override
+	public int getNum() {
+		return NUM * num;
+	}
+	
+	void child2() {
+		System.out.println("MyChild2 child2() 메서드 ");
+	}
+}
+
+public class Ex03_MyInterTest {
+
+	public static void main(String[] args) {
+		// 생성
+		Ex03_MyInterface ch1 = new MyChild1();
+		System.out.println("main ch1.getName() => " + ch1.getName());
+		System.out.println("main ch1.getNum() => " + ch1.getNum());
+		ch1.getName();
+		ch1.getNum();
+		// ch1.child1(); // 인터페이스에 정의 되지않은 멤버는 접근불가
+		
+		Ex03_MyInterface ch2 = new MyChild2();
+		System.out.println("main ch2.getName() => " + ch2.getName());
+		System.out.println("main ch2.getNum() => " + ch2.getNum());
+		ch2.getName();
+		ch2.getNum();
+		// ch2.child1(); // 인터페이스에 정의 되지않은 멤버는 접근불가
+		
+		// => 정리
+		// MyChild1, MyChild2 는 서로 무관하지만 interface 로 인해 같은 Type 으로 사용가능
+		// 다형성 적용 가능해짐
+		ch2 = ch1;
+		System.out.println("main ch2.getName() => " + ch2.getName());
+		System.out.println("main ch2.getNum() => " + ch2.getNum());
+		
+		// 비교
+		MyChild1 mch1 = new MyChild1();
+		MyChild2 mch2 = new MyChild2();
+		// mch2 = mch1; // 서로 다른 Type 이므로 오
+	}
+
+}
