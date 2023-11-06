4f1b0675ff54d869d4442e8d8039e847ed80e736
Nam39rou7d <wlsgnl502@gmail.com>
2023-11-06 17:40:52 +0900

    20231106 AOP

--- mTest/Mywork/Spring02/src/main/java/mapperInterface/MemberMapper.java
+++ mTest/Mywork/Spring02/src/main/java/mapperInterface/MemberMapper.java
@@ -2,11 +2,42 @@ package mapperInterface;
 
 import java.util.List;
 
-import domain.BoardDTO;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+//import criTest.SearchCriteria;
 import domain.MemberDTO;
 
 public interface MemberMapper {
-
+	
+	// Junit Test
+	// => @Select 적용 연습
+	@Select("select * from member where id=#{id}")
+	MemberDTO selectOneID(String id);
+	
+	// @Param 적용 Test
+	//  -> 기본규칙 : Mybatis 에서는 매개변수 Type은 무관하지만, 개수는 1개만 허용
+	//  -> @Param : mapper 에서 #{...} 적용, 복수개수 사용 가능 (단, 기본자료형은 사용불가)
+	@Select("select * from member where id=#{ii} and jno=#{jno}")
+	MemberDTO selectOneJno(@Param("ii") String id, @Param("jno") Integer jno);
+	
+	// => totalCount (Group 함수 Test)
+	@Select("select count(*) from member where id != 'admin'")
+	int totalCount();
+	
+	/*	
+	// ** Member SearchCri_PageList *******************
+	List<MemberDTO> searchList(SearchCriteria cri);
+	int searchTotalCount(SearchCriteria cri);
+	
+	// ** Member Check_List
+	List<MemberDTO> mcheckList(SearchCriteria cri);
+	int checkTotalCount(SearchCriteria cri);
+	
+	// ** JoInfo_List 추가 
+	List<MemberDTO> joInfoList();
+*/
+	
 	// ** Jo_List 추가
 	List<MemberDTO> joList(int jno);
 	
@@ -20,6 +51,7 @@ public interface MemberMapper {
 	int insert(MemberDTO dto);
 
 	// ** update
+	// => id (P.Key) 제외한 모든 컬럼수정
 	int update(MemberDTO dto);
 
 	// ** delete
